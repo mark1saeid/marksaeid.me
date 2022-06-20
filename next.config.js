e888a8d1f9ff66270cc79f20abd254bf58a84321
Mark Saeid <marksaeid10@gmail.com>
2022-06-20 17:57:35 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -3,50 +3,50 @@ const withTM = require("next-transpile-modules")(["react-github-btn"]);
 const withPWA = require("next-pwa");
 
 module.exports = withPWA(
-  withTM({
-    reactStrictMode: true,
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ["@svgr/webpack"],
-      });
+    withTM({
+        reactStrictMode: true,
+        eslint: {
+            ignoreDuringBuilds: true,
+        },
+        webpack(config) {
+            config.module.rules.push({
+                test: /\.svg$/,
+                use: ["@svgr/webpack"],
+            });
 
-      return config;
-    },
-    images: {
-      domains: ["i.scdn.co"],
-    },
-    async rewrites() {
-      return [
-        {
-          source: "/feed",
-          destination: "/feed.xml",
+            return config;
         },
-      ];
-    },
+        images: {
+            domains: ["i.scdn.co"],
+        },
+        async rewrites() {
+            return [{
+                source: "/feed",
+                destination: "/feed.xml",
+            }, ];
+        },
 
-    async redirects() {
-      return [
-        {
-          source: "/new-post",
-          destination: "/blog/10-ui-ux-tips-that-you-should-know-forms",
-          permanent: false,
+        async redirects() {
+            return [{
+                    source: "/new-post",
+                    destination: "/blog/10-ui-ux-tips-that-you-should-know-forms",
+                    permanent: false,
+                },
+                {
+                    source: "/new",
+                    destination: "/blog/10-ui-ux-tips-that-you-should-know-forms",
+                    permanent: false,
+                },
+                {
+                    source: "/newest-post",
+                    destination: "/blog/10-ui-ux-tips-that-you-should-know-forms",
+                    permanent: false,
+                },
+            ];
         },
-        {
-          source: "/new",
-          destination: "/blog/10-ui-ux-tips-that-you-should-know-forms",
-          permanent: false,
+        pwa: {
+            dest: "public",
+            disable: process.env.NODE_ENV === "development",
         },
-        {
-          source: "/newest-post",
-          destination: "/blog/10-ui-ux-tips-that-you-should-know-forms",
-          permanent: false,
-        },
-      ];
-    },
-    pwa: {
-      dest: "public",
-      disable: process.env.NODE_ENV === "development",
-    },
-  }),
+    }),
 );
